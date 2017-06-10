56be5b6803c2d428f7390c6e1c09cc33b3cfe612
George Nairooze <gnairooze@gmail.com>
2017-06-10 21:41:37 +0200

    fix result tech and business error description paramters

--- Mora.Common/Mora.Common.Vault/Result.cs
+++ Mora.Common/Mora.Common.Vault/Result.cs
@@ -15,10 +15,10 @@ namespace Mora.Common.Vault
         public const string RESULT_FAILED_DESCRIPTION = "failed";
 
         public const string RESULT_GENERAL_TECH_ERROR_CODE = "-2";
-        public const string RESULT_GENERAL_TECH_DESCRIPTION = "general technical error";
+        public const string RESULT_GENERAL_TECH_ERROR_DESCRIPTION = "general technical error";
 
         public const string RESULT_GENERAL_BUSINESS_ERROR_CODE = "-3";
-        public const string RESULT_GENERAL_BUSINESS_DESCRIPTION = "general business error";
+        public const string RESULT_GENERAL_BUSINESS_ERROR_DESCRIPTION = "general business error";
 
         public const string RESULT_SKIPPED_CODE = "-4";
         public const string RESULT_SKIPPED_DESCRIPTION = "skipped";
