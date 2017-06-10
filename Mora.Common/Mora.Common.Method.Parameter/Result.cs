2cbc28f92fdf5dcaec9264f44ac1cd75fbfd6a4e
George Nairooze <gnairooze@gmail.com>
2017-06-10 21:16:01 +0200

    add result to git

---
+++ Mora.Common/Mora.Common.Method.Parameter/Result.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mora.Common.Method.Parameter
+{
+    public class Result<T>
+    {
+        #region properties
+        public long ID { get; set; }
+        public Guid BusinessID { get; set; }
+        public string Code { get; set; }
+        public string Description { get; set; }
+        public T Data { get; set; }
+        #endregion
+    }
+}
